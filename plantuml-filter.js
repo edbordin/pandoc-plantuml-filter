bb6104ca84a2417cb0e7e7d782f41fb9bf66eafc
Edward Bordin <edwardb@alertedh.com>
2018-03-07 15:45:19 +1100

    Add crude render caching, add captions and fig:label support, upgrade bundled plantuml jar

--- plantuml-filter.js
+++ plantuml-filter.js
@@ -3,8 +3,10 @@ var fs = require('fs');
 var execSync = require("child_process").execSync;
 var path = require('path');
 var util = require('util');
+var crypto = require('crypto');
 
 var imgCounter = 0;
+var tempDirName = ".temp";
 
 function filterPlantuml(type, value, format, meta) {
     if (type !== 'CodeBlock') {
@@ -17,25 +19,45 @@ function filterPlantuml(type, value, format, meta) {
     }
 
     var umlText = value[1];
-
-    var plantumlPath = path.join(__dirname, "plantuml.8036.jar");
-    var res = execSync(util.format("java -splash:no -jar \"%s\" -charset UTF-8 -pipe", plantumlPath), { input: umlText });
-    var tempDirName = ".temp";
-    if (!fs.existsSync(tempDirName)) {
-        fs.mkdirSync(tempDirName);
+    
+    var lines = umlText.split(/\r?\n/);
+    
+    // use the SHA256 hash of the plantuml code to detect whether we need to re-render this diagram or not
+    // this is not perfect since:
+    // 1. It doesn't account for external included files changing
+    // 2. It doesn't clean up after itself
+    // Both of those problems can be fixed by deleting the temp folder manually
+    
+    var fileName = util.format("%s.png", crypto.createHash('sha256').update(umlText).digest("hex"));
+    var filePath = path.join(tempDirName, fileName);
+    
+    var caption = [];
+    var figname = 'fig:'+fileName;
+    
+    // if there is at least one line before @startuml, use this as the caption for the generated image
+    if (lines.length > 0 && 0 > lines[0].indexOf('@startuml')) {
+        caption = [pandoc.Str(lines[0])];
+    }
+    
+    // if there are at least two lines before @startuml, use the second (with 'fig:' prepended) as the figure name for the generated image
+    if (lines.length > 1 && 0 > lines[1].indexOf('@startuml')) {
+        figname = lines[1];
+    }
+    
+    if (!fs.existsSync(filePath)) {
+        var plantumlPath = path.join(__dirname, "plantuml.1.2018.0.jar");
+        var res = execSync(util.format("java -splash:no -jar \"%s\" -charset UTF-8 -tpng -pipe", plantumlPath), { input: umlText });
+        fs.writeFileSync(filePath, res);
     }
 
-    var fileName = util.format("%d.png", imgCounter++);
-    var filePath = path.join(tempDirName, fileName);
-    fs.writeFileSync(filePath, res);
-
-    return pandoc.Para(
+    var res = pandoc.Para(
         [
             pandoc.Image(
-                ['', [], []],
-                [], // Description under image
-                [filePath, '' /* hint */])
+                [figname, [], []],
+                caption, // Description under image
+                [filePath, 'fig:' /* hint */])
         ]);
+    return res;
 }
 
 // var data = JSON.parse(json);
@@ -57,5 +79,17 @@ function filterPlantuml(type, value, format, meta) {
 // 
 //     console.log("The file was saved!");
 // }); 
+
+if (!fs.existsSync(tempDirName)) {
+        fs.mkdirSync(tempDirName);
+    }
+    
+// this code cleans up the whole temp folder
+// [edbordin] TODO: modify this to just remove the generated images that are no longer used and uncomment it
+// for (const file of fs.readdirSync(tempDirName)) {
+    // fs.unlink(path.join(tempDirName, file), err => {
+      // if (err) throw err;
+    // });
+  // }
 
 pandoc.toJSONFilter(filterPlantuml);
